f9ad4b1f3d3bd9dcd220c1296bec80c5bcd6474e
saysa <sbo@conamed.de>
2017-09-11 16:02:05 +0200

    use the admin layout

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -3,8 +3,17 @@
 namespace app\Controller;
 
 use Framework\Controller;
+use Framework\View;
 
 class AuthController extends Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->_layoutView = new View( array(
+			"file" => "layouts/admin.html.twig",
+		) );
+	}
 
 	public function loginAction() {
 
