dadcccfddb8d195c59755c6e1f004418b76def5e
saysa <sbo@conamed.de>
2017-09-11 16:24:46 +0200

    set error_login and error_password with Getters and Setters

--- Helper/Auth.php
+++ Helper/Auth.php
@@ -3,4 +3,35 @@
 namespace Helper;
 
 class Auth {
+
+	protected $_error_login;
+	protected $_error_password;
+
+	/**
+	 * @return mixed
+	 */
+	public function getErrorLogin() {
+		return $this->_error_login;
+	}
+
+	/**
+	 * @param mixed $error_login
+	 */
+	public function setErrorLogin( $error_login ) {
+		$this->_error_login = $error_login;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getErrorPassword() {
+		return $this->_error_password;
+	}
+
+	/**
+	 * @param mixed $error_password
+	 */
+	public function setErrorPassword( $error_password ) {
+		$this->_error_password = $error_password;
+	}
 }
