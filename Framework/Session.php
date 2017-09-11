321aa469696ea4fcc1b7314c9d4bebf22bd62c0a
saysa <sbo@conamed.de>
2017-09-11 17:42:29 +0200

    allow to get a Session variable by key

--- Framework/Session.php
+++ Framework/Session.php
@@ -10,6 +10,23 @@ class Session {
 		session_start();
 	}
 
+	/**
+	 * Get a Session variable by key
+	 *
+	 * @param $key
+	 * @param null $default
+	 *
+	 * @return null | mixed
+	 */
+	public function get( $key, $default = null ) {
+
+		if ( isset( $_SESSION[ $this->_prefix . $key ] ) ) {
+			return $_SESSION[ $this->_prefix . $key ];
+		}
+
+		return $default;
+	}
+
 	public function __destruct() {
 		session_commit();
 	}
