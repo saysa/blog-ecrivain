c82ee8aa14049c3fbab648a32773fe5a4624c954
saysa <sbo@conamed.de>
2017-09-12 08:58:48 +0200

    give a name for all the Routes

--- app/configuration/Routes.php
+++ app/configuration/Routes.php
@@ -12,36 +12,43 @@ class Routes {
 
 	public static $routes = array(
 		array(
+			"name"       => "home",
 			"pattern"    => "",
 			"controller" => "homepage",
 			"action"     => "index",
 		),
 		array(
+			"name"       => "blog",
 			"pattern"    => "blog",
 			"controller" => "blog",
 			"action"     => "index",
 		),
 		array(
+			"name"       => "add-a-blog-post",
 			"pattern"    => "blog/add",
 			"controller" => "blog",
 			"action"     => "add",
 		),
 		array(
+			"name"       => "edit-a-blog-post",
 			"pattern"    => "blog/edit/:id",
 			"controller" => "blog",
 			"action"     => "edit",
 		),
 		array(
+			"name"       => "delete-a-blog-post",
 			"pattern"    => "blog/delete/:id",
 			"controller" => "blog",
 			"action"     => "delete",
 		),
 		array(
+			"name"       => "show-a-blog-post",
 			"pattern"    => "blog/:id",
 			"controller" => "blog",
 			"action"     => "show",
 		),
 		array(
+			"name"       => "admin-login",
 			"pattern"    => "_admin_/login",
 			"controller" => "auth",
 			"action"     => "login",
